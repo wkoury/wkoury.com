6b781e1eaceb63ed7780cb978fd3cde9c10f289f
Will Koury <wkoury@hey.com>
2020-10-20 21:55:00 -0500

    Total redesign of everything

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Index() {
   return (
@@ -8,12 +8,23 @@ export default function Index() {
         <title>Will Koury</title>
       </Head>
       
-      <h1 className={styles.title}>
-         Will Koury
-      </h1>
-
       <main className={styles.main}>
-          
+        <h1 className={styles.title}>
+          Will Koury
+        </h1>
+
+        <p>I am a software developer & computer science undergraduate student.</p>
+        <p>I mostly build web apps. My work includes <a 
+            href="https://www.tossupapp.com/" 
+            rel="noopener noreferrer" 
+            target="_blank">
+            Tossup</a> and
+            the <a href="https://wkoury.github.io/harvard-admissions/" rel="noopener noreferrer" target="_blank">Harvard
+            Admissions Calculator</a>.
+    </p>
+        <p>I am on <a href="mailto:wkoury@hey.com">Email</a>, <a href="https://github.com/wkoury" rel="noopener noreferrer" target="_blank">GitHub</a>, and <a
+        href="https://www.linkedin.com/in/will-koury-9b0241188/" rel="noopener noreferrer" target="_blank">LinkedIn</a>.
+    </p>
       </main>
     </div>
   )
--- pages/posts.js
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function Posts() {
-    return(
-        <div>
-            <h1><Link href="/">Will Koury</Link></h1>
-        </div>
-    );
-}
