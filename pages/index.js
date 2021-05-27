92ab235f820607dc22f22eb42ee6dae74cffb1ce
Will Koury <wkoury@hey.com>
2021-05-26 22:11:52 -0400

    Lighthouse improvements
    ...and get rid of those spaces!!

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,54 @@
+import { useEffect } from "react";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
 export default function Index() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Will Koury</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="description" content="Computer science undergraduate student" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      
-      <main className={styles.main}>
-        <div className={styles.title}>
-          <h1>
-            Will Koury
-          </h1>
-          <div className={styles.profile}>
-            <img
-              src="/me.jpeg"
-              alt="Will Koury"
-            />
-          </div>
-        </div>
+	useEffect(() => {
+		document.documentElement.lang = "en-us";
+	});
 
-        <p>Software developer & computer science undergraduate student.</p>
-        <p>My work includes <a 
-            href="https://www.tossupapp.com/" 
-            rel="noopener noreferrer" 
-            target="_blank">
-            Tossup</a> and
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Will Koury</title>
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+				<meta name="description" content="Computer science undergraduate student" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<main className={styles.main}>
+				<div className={styles.title}>
+					<h1>
+						Will Koury
+          </h1>
+					<div className={styles.profile}>
+						<img
+							src="/me.jpeg"
+							alt="Will Koury"
+							loading="lazy"
+						/>
+					</div>
+				</div>
+
+				<p>Software developer & computer science undergraduate student.</p>
+				<p>My work includes <a
+					href="https://www.tossupapp.com/"
+					rel="noopener noreferrer"
+					target="_blank">
+					Tossup</a> and
             the <a href="https://wkoury.github.io/harvard-admissions/" rel="noopener noreferrer" target="_blank">Harvard
             Admissions Calculator</a>.
         </p>
-        <div className={styles.contact}>
-          <span>
-            <a href="mailto:wkoury@hey.com">📧</a>
-            {" • "}
-            <a href="https://github.com/wkoury" rel="noopener noreferrer" target="_blank">💾</a>
-            {" • "}
-            <a href="https://twitter.com/will_koury" rel="noopener noreferrer" target="_blank">🐦</a>
-          </span>
-        </div>
-      </main>
-    </div>
-  );
+				<div className={styles.contact}>
+					<span>
+						<a href="mailto:wkoury@hey.com">📧</a>
+						{" • "}
+						<a href="https://github.com/wkoury" rel="noopener noreferrer" target="_blank">💾</a>
+						{" • "}
+						<a href="https://twitter.com/will_koury" rel="noopener noreferrer" target="_blank">🐦</a>
+					</span>
+				</div>
+			</main>
+		</div>
+	);
 }
