b93f50cb34522e8e91390ea5254492f3b6f68346
Will Koury <wkoury@hey.com>
2022-04-29 14:45:16 -0500

    Redirect `/capstone` to our presentation

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+	async redirects() {
+	  return [
+		{
+		  source: '/capstone',
+		  destination: 'https://cs4273-final-presentation-a5w3bw5hcq-uc.a.run.app/#1',
+		  permanent: false
+		}
+	  ];
+	}
+  };
